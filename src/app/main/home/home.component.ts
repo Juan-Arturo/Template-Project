ab2bc8ea4043f33b75993df6cf75ed427240e67a
Juan-Arturo <jagpjagp002@gmail.com>
2025-01-03 17:30:20 -0600

    modificacion

--- src/app/main/home/home.component.ts
+++ src/app/main/home/home.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
-import { PaginatorComponent } from '../../shared/paginator/paginator.component';
 import { FooterComponent } from '../../shared/footer/footer.component';
 import { HeaderComponent } from '../../shared/header/header.component';
 import { Footer2Component } from '../../shared/footer2/footer2.component';
 import { SidebarComponent } from '../../shared/sidebar/sidebar.component';
 
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [PaginatorComponent, HeaderComponent, FooterComponent, Footer2Component, SidebarComponent],
+  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from '../shared/sidebar/sidebar.component';
+import { FooterComponent } from '../shared/footer/footer.component';
+import { HeaderComponent } from '../shared/header/header.component';
+import { Footer2Component } from '../shared/footer2/footer2.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeaderComponent,
+      FooterComponent, Footer2Component, SidebarComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
