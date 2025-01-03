19aea5f4508c16aade087cf63d7085dbad8341d1
Juan-Arturo <jagpjagp002@gmail.com>
2025-01-03 17:41:13 -0600

    modificacion de main

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -4,12 +4,13 @@ import { SidebarComponent } from '../shared/sidebar/sidebar.component';
 import { FooterComponent } from '../shared/footer/footer.component';
 import { HeaderComponent } from '../shared/header/header.component';
 import { Footer2Component } from '../shared/footer2/footer2.component';
+import { Header2Component } from '../shared/header2/header2.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
   imports: [RouterOutlet,HeaderComponent,
-      FooterComponent, Footer2Component, SidebarComponent],
+      FooterComponent, Footer2Component, SidebarComponent,Header2Component],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
