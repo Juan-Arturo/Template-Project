24c6e190ece2b3794ec766e95de2a65317f57bed
Gabriel Cruz <superbal27@gmail.com>
2025-01-02 23:44:06 -0600

    Cambios UI

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { PaginatorComponent } from '../shared/paginator/paginator.component';
-import { FoterVdaComponent } from '../shared/foter-vda/foter-vda.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [RouterOutlet,PaginatorComponent,FoterVdaComponent],
+  imports: [RouterOutlet],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
