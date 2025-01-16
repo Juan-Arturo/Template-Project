385f1910e136a94cba80c348edaba3c7c062c579
Gabriel Cruz <superbal27@gmail.com>
2025-01-16 09:46:21 -0600

    IU - 16/01/2025

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './main/home/home.component';
+import { NewslatterComponent } from './main/newslatter/newslatter.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'home', component: HomeComponent },
+    { path: 'newslatter', component: NewslatterComponent },
     { path: '**', component: HomeComponent },
 ];
