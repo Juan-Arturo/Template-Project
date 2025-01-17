f8bc104d4fbcf22ac032cd5ac14caf01385479ef
Gabriel Cruz <superbal27@gmail.com>
2025-01-17 16:50:41 -0600

    Tables IU

--- src/app/main/home/home.component.ts
+++ src/app/main/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { LoginComponent } from '../../shared/login/login.component';
+import { TablesComponent } from "../../shared/tables/tables.component";
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [LoginComponent],
+  imports: [LoginComponent, TablesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../shared/header/header.component';
 import { SidebarComponent } from '../shared/sidebar/sidebar.component';
 import { FooterComponent } from '../shared/footer/footer.component';
+import { TablesComponent } from '../shared/tables/tables.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, SidebarComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, SidebarComponent, FooterComponent, TablesComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
